f7d60c2fe83254d2c0da129b486c35823de22be7
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-03-15 18:05:55 +0100

    [config] export default - for now
    [config] adding additional test for high precision trunc
    
    [config] adding some defaults

--- config/default.py
+++ config/default.py
@@ -4,15 +4,28 @@
 # Copyright 2018 Datadog, Inc.
 
 # Defaults
+
+DEFAULT_ADDITIONAL_CHECKSD = "/etc/datadog-agent/checks.d"
+DEFAULT_DD_URL = "https://app.datadoghq.com"
+DEFAULT_FORWARDER_TO = 20
+DEFAULT_FORWARDER_RETRY_Q_MAX_SIZE = 30
+DEFAULT_HOST_METADATA_INTERVAL = 4 * 60 * 60
+DEFAULT_EXT_HOST_TAGS_INTERVAL = 5 * 60
+DEFAULT_LOG_LEVEL = 'info'
+
+
 def init(config):
     config_defaults = {
-        "dd_url": "https://app.datadoghq.com",
+        "dd_url": DEFAULT_DD_URL,
         "app_key": "",
         "hostname": "",
         "tags": [],
-        "log_level": "info",
-        "forwarder_timeout": 20,
-        "forwarder_retry_queue_max_size": 30,
+        "log_level": DEFAULT_LOG_LEVEL,
+        "forwarder_timeout": DEFAULT_FORWARDER_TO,
+        "forwarder_retry_queue_max_size": DEFAULT_FORWARDER_RETRY_Q_MAX_SIZE,
+        "additional_checksd": DEFAULT_ADDITIONAL_CHECKSD,
+        "host_metadata": DEFAULT_HOST_METADATA_INTERVAL,
+        "external_host_tags": DEFAULT_EXT_HOST_TAGS_INTERVAL,
     }
 
     for k, v in config_defaults.iteritems():

ba462e4aeb40018db1e8910ae1da44423db66cb9
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-03-23 17:00:28 -0400

    [config] adding some defaults

--- config/default.py
+++ config/default.py
@@ -7,6 +7,11 @@
 
 DEFAULT_ADDITIONAL_CHECKSD = "/etc/datadog-agent/checks.d"
 DEFAULT_DD_URL = "https://app.datadoghq.com"
+DEFAULT_FORWARDER_TO = 20
+DEFAULT_FORWARDER_RETRY_Q_MAX_SIZE = 30
+DEFAULT_HOST_METADATA_INTERVAL = 4 * 60 * 60
+DEFAULT_EXT_HOST_TAGS_INTERVAL = 5 * 60
+DEFAULT_LOG_LEVEL = 'info'
 
 
 def init(config):
@@ -15,10 +20,12 @@ def init(config):
         "app_key": "",
         "hostname": "",
         "tags": [],
-        "log_level": "info",
-        "forwarder_timeout": 20,
-        "forwarder_retry_queue_max_size": 30,
+        "log_level": DEFAULT_LOG_LEVEL,
+        "forwarder_timeout": DEFAULT_FORWARDER_TO,
+        "forwarder_retry_queue_max_size": DEFAULT_FORWARDER_RETRY_Q_MAX_SIZE,
         "additional_checksd": DEFAULT_ADDITIONAL_CHECKSD,
+        "host_metadata": DEFAULT_HOST_METADATA_INTERVAL,
+        "external_host_tags": DEFAULT_EXT_HOST_TAGS_INTERVAL,
     }
 
     for k, v in config_defaults.iteritems():

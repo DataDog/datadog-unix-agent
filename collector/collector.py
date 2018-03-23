94d77f172de3295b1f8ac88dc26da77547f04925
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-03-23 18:35:47 -0400

    [collector] make more resilient to multiple calls + meta things

--- collector/collector.py
+++ collector/collector.py
@@ -4,11 +4,15 @@
 # Copyright 2018 Datadog, Inc.
 
 from collections import defaultdict
+import time
 import logging
 
+from config.providers import ConfigProvider
 from . import CheckLoader, WheelLoader
 from .wheel_loader import DD_WHEEL_NAMESPACE
 
+from metadata import get_metadata
+from utils.hostname import get_hostname
 
 log = logging.getLogger(__name__)
 
@@ -19,12 +23,21 @@ class Collector(object):
         self._config = config
         self._providers = {}
         self._loaders = []
-        self._check_configs = defaultdict(list)
+        self._check_configs = defaultdict(dict)
         self._check_classes = {}
         self._check_classes_errors = {}
         self._check_instances = defaultdict(list)
+        self._check_instance_signatures = set()
+        self._hostname = get_hostname()
+        self._metadata = {
+            'host_metadata': {
+                'last': time.time(),
+                'interval': int(config.get('metadata_interval')),
+                'meta': None,
+            },
+        }
 
-        super(Collector, self).__init__()
+        self.set_loaders()
 
     def set_loaders(self):
         self._loaders.append(WheelLoader(namespace=DD_WHEEL_NAMESPACE))
@@ -32,38 +45,64 @@ class Collector(object):
         check_loader.add_place(self._config['additional_checksd'])
         self._loaders.append(check_loader)
 
+    def add_provider(self, id, provider):
+        if not isinstance(provider, ConfigProvider):
+           raise ValueError("expected a configuration provider")
+
+        if id in self._providers:
+            return
+
+        self._providers[id] = provider
+
     def collect_check_configs(self):
-        for provider in self._providers:
+        for source, provider in self._providers.iteritems():
             checksconfigs = provider.collect()
             for check, configs in checksconfigs:
-                self._check_configs[check] = self._check_configs.extend(configs)
+                current_configs = self._check_configs.get(source).get(check, [])
+                for config in configs:
+                    if config in current_configs:
+                        # skip existing ones in case we re-call this
+                        continue
+                    current_configs.append(config)
+
+                self._check_configs[source][check] = current_configs
 
     def load_check_classes(self):
-        for check_name in self._check_configs:
-            for loader in self._loaders:
-                try:
-                    check_class, errors = loader.load(check_name)
-                    if check_class:
-                        self._check_classes[check_name] = check_class
-                    if errors:
-                        self._check_classes_errors[check_name] = errors
-                except Exception:
-                    log.exception("unexpected error loading check %s", check_name)
+        for _, check_configs in self._check_configs:
+            for check_name in check_configs:
+                if check_name in self._check_classes:
+                    continue
+
+                for loader in self._loaders:
+                    try:
+                        check_class, errors = loader.load(check_name)
+                        if check_class:
+                            self._check_classes[check_name] = check_class
+                        if errors:
+                            self._check_classes_errors[check_name] = errors
+                    except Exception:
+                        log.exception("unexpected error loading check %s", check_name)
 
     def instantiate_checks(self):
-        for check_name, configs in self._check_configs.iteritems():
-            check_class = self._check_classes.get(check_name)
-            if check_class:
-                for config in configs:
-                    init_config = config.get('init_config', {})
-                    instances = config.get('instances')  # should be single instance
-                    for instance in instances:
-                        try:
-                            check_instance = check_class(check_name, init_config, instance)
-                            self._check_instances[check_name].append(check_instance)
-                        except Exception:
-                            log.error("unable to instantiate instance %s for %s",
-                                      instance, check_name)
+        for source, check_configs in self._check_configs:
+            for check_name, configs in check_configs.iteritems():
+                check_class = self._check_classes.get(check_name)
+                if check_class:
+                    for config in configs:
+                        init_config = config.get('init_config', {})
+                        instances = config.get('instances')  # should be single instance
+                        for instance in instances:
+                            signature = (init_config, instance)
+                            if signature in self._check_instance_signatures:
+                                continue
+
+                            try:
+                                check_instance = check_class(check_name, init_config, instance)
+                                self._check_instances[check_name].append(check_instance)
+                                self._check_instance_signatures.add(signature)
+                            except Exception:
+                                log.error("unable to instantiate instance %s for %s",
+                                        instance, check_name)
 
     def run_checks(self):
         for name, checks in self._check_instances.iteritems():
@@ -73,3 +112,11 @@ class Collector(object):
                     check.run(check.instance)
                 except Exception:
                     log.exception("error for instance: %s", str(check.instance))
+
+    def refresh_metadata(self):
+        now = time.time()
+        if (self._metadata['host_metadata']['last'] +
+            self._metadata['host_metadata']['interval']) >= now:
+            self._metadata['host_metadata']['meta']= get_metadata()
+            self._metadata['host_metadata']['last']= now
+            # set the updated metadata somewhere... Serializer?

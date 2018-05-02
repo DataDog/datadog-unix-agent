b9407c758a33392ab7e6a66bafe99cdb56a658e9
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-05-02 18:09:56 +0200

    [load] fixing 1,5,15 second avg load

--- checks/corechecks/system/tests/test_load.py
+++ checks/corechecks/system/tests/test_load.py
@@ -5,15 +5,17 @@
 
 import mock
 
+from checks.corechecks.system import load
 from aggregator import MetricsAggregator
 
 GAUGE = 'gauge'
+
+AIX_MOCK_LOAD = '  10:50AM   up 8 days,   2:48,  2 users,  load average: 1.19, 0.77, 0.85'
 
 
 @mock.patch("psutil.cpu_count", return_value=2)
 @mock.patch("os.getloadavg", return_value=(0.42, 0.43, 0.49))
 def test_load(getloadavg, cpu_count):
-    from checks.corechecks.system import load
 
     hostname = 'foo'
     aggregator = MetricsAggregator(
@@ -48,7 +50,6 @@ def test_load(getloadavg, cpu_count):
 @mock.patch("psutil.cpu_count", return_value=0)
 @mock.patch("os.getloadavg", return_value=(0.42, 0.43, 0.49))
 def test_load_no_cpu_count(getloadavg, cpu_count):
-    from checks.corechecks.system import load
 
     hostname = 'foo'
     aggregator = MetricsAggregator(
@@ -71,6 +72,41 @@ def test_load_no_cpu_count(getloadavg, cpu_count):
         'system.load.1': (GAUGE, 0.42),
         'system.load.5': (GAUGE, 0.43),
         'system.load.15': (GAUGE, 0.49),
+    }
+
+    assert len(metrics) == len(expected_metrics)
+    for metric in metrics:
+        assert metric['metric'] in expected_metrics
+        assert len(metric['points']) == 1
+        assert metric['host'] == hostname
+        assert metric['type'] == expected_metrics[metric['metric']][0]
+        assert metric['points'][0][1] == expected_metrics[metric['metric']][1]
+
+
+@mock.patch("psutil.cpu_count", return_value=2)
+@mock.patch("checks.corechecks.system.load.get_subprocess_output", return_value=(AIX_MOCK_LOAD, None, None))
+@mock.patch("os.getloadavg", side_effect=AttributeError("'module' object has no attribute 'getloadavg')"))
+def test_load_aix(getloadavg, get_subprocess_output, cpu_count):
+
+    hostname = 'foo'
+    aggregator = MetricsAggregator(
+        hostname,
+        interval=1.0,
+        histogram_aggregates=None,
+        histogram_percentiles=None,
+    )
+
+    c = load.Load("load", {}, {}, aggregator)
+    c.check({})
+    metrics = c.aggregator.flush()
+
+    expected_metrics = {
+        'system.load.1': (GAUGE, 1.19),
+        'system.load.5': (GAUGE, 0.77),
+        'system.load.15': (GAUGE, 0.85),
+        'system.load.norm.1': (GAUGE,0.595),
+        'system.load.norm.5': (GAUGE, 0.385),
+        'system.load.norm.15': (GAUGE, 0.425),
     }
 
     assert len(metrics) == len(expected_metrics)

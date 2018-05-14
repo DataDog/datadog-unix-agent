60ac1fa103b186ed7098e75c8d26155e6fcca629
Jaime Fullaondo <truthbk@gmail.com>
2018-05-14 16:03:17 +0200

    [uptime] fix uptime check (#20)
    * [process] adding subprocess collection facilities
    
    * [uptime] fixing uptime collection for AIX
    
    * [uptime] add clarifying comment

--- checks/corechecks/system/uptime_check.py
+++ checks/corechecks/system/uptime_check.py
@@ -4,10 +4,34 @@
 # Copyright 2018 Datadog, Inc.
 
 import uptime
+
+from utils.process import get_subprocess_output
 from checks import AgentCheck
 
 
 class UptimeCheck(AgentCheck):
 
     def check(self, instance):
-        self.gauge("system.uptime", uptime.uptime())
+        up = uptime.uptime()
+        if up:
+            self.gauge("system.uptime", up)
+            return
+
+        # On AIX and some other platforms the uptime module may fail to find the system
+        # uptime and return `None` - in that case, grab the uptime as the init process
+        # (pid 1) uptime
+        try:
+            # get uptime from init process lifetime (pid 1)
+            # format: 8-00:56:09
+            up, _, _ = get_subprocess_output(['ps', '-o', 'etime=', '-p1'], self.log)
+            up = up.split('-')
+            days, rest = up[0], up[1]
+
+            time = rest.split(':')
+            days_s = int(days) * 24 * 60 * 60
+            hour_s = int(time[0]) * 60 * 60
+            mins_s = int(time[1]) * 60
+            secs = int(time[2])
+            self.gauge("system.uptime", days_s+hour_s+mins_s+secs)
+        except Exception:
+            self.log.exception("Cannot collect uptime statistics")

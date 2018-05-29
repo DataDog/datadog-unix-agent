019c92e0af333b29e5d99045ea459bf0745bbe54
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-05-29 15:16:29 +0200

    [dogstatsd] fixing interpreter to be virtual env compatible

--- dogstatsd.py
+++ dogstatsd.py
@@ -1,4 +1,4 @@
-#!/opt/datadog-agent/embedded/bin/python
+#!/usr/bin/env python
 
 # Unless explicitly stated otherwise all files in this repository are licensed
 # under the Apache License Version 2.0.

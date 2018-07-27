8106191b001ee1fe6db44d49bba6c3e916849fb5
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-07-27 16:14:35 +0200

    [iostat] actually add check to collector

--- collector/collector.py
+++ collector/collector.py
@@ -17,7 +17,7 @@ log = logging.getLogger(__name__)
 
 
 class Collector(object):
-    CORE_CHECKS = ['cpu', 'filesystem', 'iostat','load', 'memory', 'uptime']
+    CORE_CHECKS = ['cpu', 'filesystem', 'iostat', 'load', 'memory', 'uptime']
 
     def __init__(self, config, aggregator=None):
         self._config = config
@@ -56,9 +56,10 @@ class Collector(object):
             UptimeCheck
         )
         self._check_classes['cpu'] = Cpu
+        self._check_classes['filesystem'] = Filesystem
+        self._check_classes['iostat'] = IOStat
         self._check_classes['load'] = Load
         self._check_classes['memory'] = Memory
-        self._check_classes['filesystem'] = Filesystem
         self._check_classes['uptime'] = UptimeCheck
 
     def load_check_classes(self):

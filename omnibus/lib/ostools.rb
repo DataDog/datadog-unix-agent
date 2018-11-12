4b2e78d1d742b342c5f09c305376c8443c5a671a
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-11-12 01:45:22 +0100

    [omnibus] adding ostools lib

---
+++ omnibus/lib/ostools.rb
@@ -0,0 +1,50 @@
+# ------------------------------------
+# OS-detection helper functions
+# ------------------------------------
+def linux?()
+    return %w(rhel debian fedora suse gentoo slackware arch exherbo).include? ohai['platform_family']
+end
+
+def redhat?()
+    return %w(rhel fedora).include? ohai['platform_family']
+end
+
+def suse?()
+    return %w(suse).include? ohai['platform_family']
+end
+
+def debian?()
+    return ohai['platform_family'] == 'debian'
+end
+
+def osx?()
+    return ohai['platform_family'] == 'mac_os_x'
+end
+
+def windows?()
+    return ohai['platform_family'] == 'windows'
+end
+
+def aix?()
+    return ohai['platform'] == 'aix'
+end
+
+def aix_env()
+    env = with_standard_compiler_flags(with_embedded_path, :aix => { :use_gcc => true }) 
+    env.merge({"LD_RUN_PATH" => "#{install_dir}/embedded/lib"})
+
+    return env
+end
+
+def os
+    case RUBY_PLATFORM
+    when /linux/
+      'linux'
+    when /darwin/
+      'mac_os'
+    when /x64-mingw32/
+      'windows'
+    else
+      raise 'Unsupported OS'
+    end
+  end

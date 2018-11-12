0c0810c249772abf1f55fabb961d5e9ad33a3265
Jaime Fullaondo <jaime.fullaondo@datadoghq.com>
2018-11-12 02:06:45 +0100

    [omnibus][xz] fixing sw definition

--- omnibus/config/software/xz.rb
+++ omnibus/config/software/xz.rb
@@ -2,7 +2,7 @@ name "xz"
 default_version "5.2.4"
 
 version "5.2.4" do
-  source sha256: "be84b8840cb1f156711bee957c613e3bd56b36bbfc43bc144d9913955a391a83"
+  source sha256: "b512f3b726d3b37b6dc4c8570e137b9311e7552e8ccbab4d39d47ce5f4177145"
 end
 
 source url: "https://tukaani.org/xz/xz-#{version}.tar.gz"
